373022c86a781e2c2c6e013336f3064d02b67e02
Francesco Ferrri <francesco.ferrari@bluewin.ch>
2018-09-29 14:59:47 +0200

    modify get_x_prev_trading_day

--- pairstrading.py
+++ pairstrading.py
@@ -3,17 +3,39 @@ Created on 29 Sep 2018
 
 @author: S.Walliser, P.Lucescu, F.Ferrari
 '''
-
 import datetime as dt
 import numpy as np
 
-from dateutil.relativedelta import relativedelta
+from nyse_holidays import get_nyse_holidays
+from dateutil import rrule 
 
-def get_start_lookback(date_, days_):
-    date_ = dt.date(2018, 8, 22)
-    days_ = 30
-    date_2 = date_ - relativedelta(date_, days=days_)
-    np.busday_offset(date_2, -1)
-    return date_2
+def get_x_prev_trading_day(date_, trad_days_):
+    out_date = np.busday_offset(date_, -252, holidays = NYSE_HOLIDAYS)
+    return out_date.astype(dt.date)
+pass
 
-# Santiago 
+# def get_nyse_holidays(year_start_, year_end_):
+#     a = dt.date(year_start_, 1, 1)
+#     b = dt.date(year_end_, 12, 31)
+#     rs = rrule.rruleset()
+#     rs.rrule(rrule.rrule(rrule.YEARLY, dtstart=a, until=b, bymonth=12, bymonthday=31, byweekday=rrule.FR)) # New Years Day  
+#     rs.rrule(rrule.rrule(rrule.YEARLY, dtstart=a, until=b, bymonth= 1, bymonthday= 1))                     # New Years Day  
+#     rs.rrule(rrule.rrule(rrule.YEARLY, dtstart=a, until=b, bymonth= 1, bymonthday= 2, byweekday=rrule.MO)) # New Years Day    
+#     rs.rrule(rrule.rrule(rrule.YEARLY, dtstart=a, until=b, bymonth= 1, byweekday= rrule.MO(3)))            # Martin Luther King Day   
+#     rs.rrule(rrule.rrule(rrule.YEARLY, dtstart=a, until=b, bymonth= 2, byweekday= rrule.MO(3)))            # Washington's Birthday
+#     rs.rrule(rrule.rrule(rrule.YEARLY, dtstart=a, until=b, byeaster= -2))                                  # Good Friday
+#     rs.rrule(rrule.rrule(rrule.YEARLY, dtstart=a, until=b, bymonth= 5, byweekday= rrule.MO(-1)))           # Memorial Day
+#     rs.rrule(rrule.rrule(rrule.YEARLY, dtstart=a, until=b, bymonth= 7, bymonthday= 3, byweekday=rrule.FR)) # Independence Day
+#     rs.rrule(rrule.rrule(rrule.YEARLY, dtstart=a, until=b, bymonth= 7, bymonthday= 4))                     # Independence Day
+#     rs.rrule(rrule.rrule(rrule.YEARLY, dtstart=a, until=b, bymonth= 7, bymonthday= 5, byweekday=rrule.MO)) # Independence Day
+#     rs.rrule(rrule.rrule(rrule.YEARLY, dtstart=a, until=b, bymonth= 9, byweekday= rrule.MO(1)))            # Labor Day
+#     rs.rrule(rrule.rrule(rrule.YEARLY, dtstart=a, until=b, bymonth=11, byweekday= rrule.TH(4)))            # Thanksgiving Day
+#     rs.rrule(rrule.rrule(rrule.YEARLY, dtstart=a, until=b, bymonth=12, bymonthday=24, byweekday=rrule.FR)) # Christmas  
+#     rs.rrule(rrule.rrule(rrule.YEARLY, dtstart=a, until=b, bymonth=12, bymonthday=25))                     # Christmas  
+#     rs.rrule(rrule.rrule(rrule.YEARLY, dtstart=a, until=b, bymonth=12, bymonthday=26, byweekday=rrule.MO)) # Christmas 
+#     # Exclude potential holidays that fall on weekends
+#     rs.exrule(rrule.rrule(rrule.WEEKLY, dtstart=a, until=b, byweekday=(rrule.SA,rrule.SU)))
+#     return list(rs)
+# pass
+
+NYSE_HOLIDAYS = get_nyse_holidays(1996, 2018)
